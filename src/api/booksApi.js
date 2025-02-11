15a44a7a670cd9f372bf79ad7d6ab8cc9781f5f8
Héctor José Díaz Sandate <hectorjosediazsandate@gmail.com>
2025-02-11 12:05:56 -0600

    fix .env

--- src/api/booksApi.js
+++ src/api/booksApi.js
@@ -1,67 +1,22 @@
-// import axios from 'axios';
-// const API_URL = process.env.EXPO_PUBLIC_API_URL;
-
-// const api = axios.create({
-//   baseURL: API_URL,
-//   timeout: 5000,
-// });
-
-
-// export const getBooks = async () => {
-//   try {
-//     const response = await api.get('/');
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error al obtener los libros:', error);
-//     throw error;
-//   }
-// };
-
-// export const getBookById = async (id) => {
-//   try {
-//     const response = await api.get(`/${id}`);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error al obtener el libro:', error);
-//     throw error;
-//   }
-// };
-
-// export const addBook = async (book) => {
-//   try {
-//     const response = await api.post('/', book);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error al agregar el libro:', error);
-//     throw error;
-//   }
-// };
-
-// export const updateBook = async (id, book) => {
-//   try {
-//     const response = await api.put(`/${id}`, book);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error al actualizar el libro:', error);
-//     throw error;
-//   }
-// };
-
-
 import axios from 'axios';
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const API_URL = process.env.EXPO_PUBLIC_API_URL;  // ✅ Correcto
 
 const api = axios.create({
   baseURL: API_URL,
   timeout: 5000,
 });
 
+
 export const getBooks = async () => {
   try {
     const response = await api.get('/books');
     return response.data;
+    console.log('API_URL:', process.env.EXPO_PUBLIC_API_URL);
+
   } catch (error) {
+    console.log('API_URL:', process.env.EXPO_PUBLIC_API_URL);
+
     console.error('Error al obtener los libros:', error);
     throw error;
   }
